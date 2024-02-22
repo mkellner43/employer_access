f9630ed4249acb6f1e28bf362b0faed436edae6b
mkellner43 <matthew.kellner@outlook.com>
2024-02-21 18:45:16 -0600

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # this needs to be changed to production host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
